ae42d9588951f7039cfe8e8d6bdd9bae633549b6
Bondhan Mondal <bondhan2725@gmail.com>
2022-02-01 20:53:34 +0600

    fibonacci series function added

--- fibonacci.js
+++ fibonacci.js
@@ -10,11 +10,39 @@ nth = (n-1)th + (n-2)th
 ith = (i-1)th + (i-2)th
 */
 
-const fibo = [0, 1];
+/* const fibo = [0, 1];
 for (let i = 2; i <= 10; i++){
     // nth = (n - 1)th + (n - 2)th
     // ith = (i - 1)th + (i - 2)th
     fibo[i] = fibo[i - 1] + fibo[i - 2]
 
 }
-console.log(fibo);
+console.log(fibo); */
+
+/* function fibonacciSeries(num){
+    const fibo = [0, 1];
+    for(let i = 2; i <= num; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo;
+}
+
+const fibo10 = fibonacciSeries(10);
+console.log(fibo10); */
+
+function fibonacciSeries(num) {
+    if(typeof(num) != 'number'){
+        return 'Error!! please give a number';
+    }
+    if (num < 2){
+        return 'Please Enter a Positive Number Grater than 1'
+    }
+    const fibo = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+
+const fibo10 = fibonacciSeries(1);
+console.log(fibo10);
--- max.js
+++ max.js
@@ -26,3 +26,37 @@ console.log('largest is : ', max);
 
 //solve task1: find largest between three numbers
 //solve task1: find smallest between three numbers
+
+
+/// Quiz Problems
+
+/* const number = -78;
+const answer = Math.abs(number);
+console.log(answer);
+
+var a = 2;
+var b = 3;
+if (a<b) { 
+    console.log('hello'); 
+}
+var c = 12.96;
+console.log(Math.floor(c));
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add("adam" + "eve"));
+
+let nm = 'bondhan';
+const out = nm.length;
+console.log(out);
+
+var sentence = "I am hardworking. I am determined. I will be a web developer.";
+var count = 0;
+for (var i = 0; i < sentence.length; i++){
+    var letter = sentence[i];
+    if (letter == 'a'){
+        count++;
+    }
+}
+console.log(count); */

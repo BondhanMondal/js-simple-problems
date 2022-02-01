c3676fba8072eaf64b51d0cf9b74bbd37cfc46b3
Bondhan Mondal <bondhan2725@gmail.com>
2022-02-01 22:49:10 +0600

    smallest element of an array added

--- largest-element.js
+++ largest-element.js
@@ -10,7 +10,7 @@ function largestElement(numbers){
     return largest;
 }
 
-const ages = [12, 54, 76, 34, 75, 32, 12];
+const ages = [12, 54, 76, 34, 10, 32, 12];
 
 const oldest = largestElement(ages);
 const oldest2 = largestElement([-12, -6, -17])
@@ -19,3 +19,17 @@ console.log('Oldest2 Person: ', oldest2);
 
 
 //find the smallest element of an array
+
+function smallestElement(arr){
+    let smallest = arr[0];
+    for (let i = 0; i < arr.length; i++){
+        const element = arr[i];
+        if (element < smallest){
+            smallest = element;
+        }
+    }
+    return smallest;
+}
+
+const smallestAge = smallestElement(ages);
+console.log('Smallest Age: ', smallestAge);
